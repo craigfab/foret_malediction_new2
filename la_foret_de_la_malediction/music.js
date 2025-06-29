28f847c6b5ec92091c8f323bc2bd4d40b417db52
unknown <craigfab@gmail.com>
2025-06-29 18:26:54 +0200

    modification music.js

--- la_foret_de_la_malediction/music.js
+++ la_foret_de_la_malediction/music.js
@@ -1,13 +1,19 @@
-
 // Liste des pistes musicales disponibles
 export const tracks = [
-    "../music/3 Hours of Relaxing Super Nintendo Music, Part 1 - SNESdrunk.mp3",
-    "../music/3 More Hours of Relaxing Super Nintendo Music - SNESdrunk.mp3",
-    "../music/Another 3 Hours of Relaxing Super Nintendo Music - SNESdrunk.mp3",
-    "../music/Good Ending - Relaxing & downbeat Sega Genesis music mix (2 hours).mp3",
-    "../music/Relaxing Super Nintendo Music, Part 4 - SNESdrunk.mp3",
-    "../music/Relaxing GenesisMegadrive Music (100 songs) - Part 1.mp3",
-    "../music/Relaxing GenesisMegadrive Music (100 songs) - Part 2.mp3"
+    "../music/3 Hours of Relaxing Super Nintendo Music, Part 1.1 - SNESdrunk (online-audio-converter.com) (1).mp3",
+    "../music/3 Hours of Relaxing Super Nintendo Music, Part 1.2 - SNESdrunk (online-audio-converter.com) (1).mp3",
+    "../music/3 More Hours of Relaxing Super Nintendo Music 2.1- SNESdrunk (online-audio-converter.com).mp3",
+    "../music/3 More Hours of Relaxing Super Nintendo Music 2.2 - SNESdrunk (online-audio-converter.com).mp3",
+    "../music/Another 3 Hours of Relaxing Super Nintendo Music 3.1 - SNESdrunk (online-audio-converter.com).mp3",
+    "../music/Another 3 Hours of Relaxing Super Nintendo Music 3.2 - SNESdrunk (online-audio-converter.com).mp3",
+    "../music/Good Ending - Relaxing & downbeat Sega Genesis music mix (2 hours) part 1 (online-audio-converter.com).mp3",
+    "../music/Good Ending - Relaxing & downbeat Sega Genesis music mix (2 hours) part 2 (online-audio-converter.com).mp3",
+    "../music/Relaxing GenesisMegadrive Music (100 songs) - Part 1.1 (online-audio-converter.com) (1) - Copie.mp3",
+    "../music/Relaxing GenesisMegadrive Music (100 songs) - Part 1.2 (online-audio-converter.com) (1).mp3",
+    "../music/Relaxing GenesisMegadrive Music (100 songs) - Part 2.1 (online-audio-converter.com) (1).mp3",
+    "../music/Relaxing GenesisMegadrive Music (100 songs) - Part 2.2 (online-audio-converter.com) (1) - Copie.mp3",
+    "../music/Relaxing Super Nintendo Music, Part 4.1- SNESdrunk (online-audio-converter.com).mp3",
+    "../music/Relaxing Super Nintendo Music, Part 4.2 - SNESdrunk (online-audio-converter.com).mp3"
 ];
 
 // Sélectionnez aléatoirement une piste
